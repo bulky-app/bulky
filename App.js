9596023af5da905daa142f82d5a13997cad7b273
Axole Maranjana <axolemaranjana4@gmail.com>
2022-09-13 14:09:11 +0200

    init

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>maranjana</Text>
       <StatusBar style="auto" />
     </View>
   );
