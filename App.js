b713ef20d4f9acf710933382763666e710eb7304
junior-03 <juniormaruping6@gmail.com>
2022-09-19 17:25:52 +0200

    searchscreen

--- App.js
+++ App.js
@@ -3,12 +3,13 @@ import React, { useEffect, useState } from "react";
 import RootNavigation from "./src/navigation/RootNavigator";
 import { NavigationContainer } from "@react-navigation/native";
 import LoginScreen from './src/screens/LoginScreen';
+import SearchScreen from'./src/screens/SearchScreen';
 
 const App = () => {
   return (
     /*component you want to see here*/ 
     // <NavigationContainer>
-    //   <RootNavigation />
+    //   <SearchScreen />
     // </NavigationContainer>
   );
 };
