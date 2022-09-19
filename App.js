12b8a3aa7b4aeb76ffba0b00f43c13111c1c12a0
BMakgopa <bethuelmakgopa@gmail.com>
2022-09-19 21:53:44 +0200

    Update

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
 import "react-native-gesture-handler";
 import React, { useEffect, useState } from "react";
 import RootNavigation from "./src/navigation/RootNavigator";
-import LoginScreen from "./src/screens/LoginScreen";
-import RegisterScreen from "./src/screens/RegisterScreen";
 import { Provider } from "react-redux";
 import store from "./src/redux/store";
-import CartScreen from "./src/screens/CartScreen";
-import EmailVerificationScreen from "./src/screens/EmailVerificationScreen";
 
 const App = () => {
   return (
