f1135829b25850ed1aaf576e35b96914e0f357f2
Axole Maranjana <82832027+Axolem@users.noreply.github.com>
2022-09-23 02:25:52 +0200

    Limit font scaling

--- App.js
+++ App.js
@@ -3,6 +3,12 @@ import store from "./src/redux/store";
 import { Provider } from "react-redux";
 import RootNavigator from "./src/navigation/RootNavigator";
 import Wallet from "./src/screens/profile/Wallet";
+import {Text, TextInput} from "react-native";
+
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.maxFontSizeMultiplier = 1; // the maximum amount the font size will scale.
+TextInput.defaultProps = Text.defaultProps || {};
+TextInput.defaultProps.maxFontSizeMultiplier = 1; /
 
 const App = () => {
   return (
