e10c72ae1811258510d9144584cdfd1859e8f3bd
Axole Maranjana <82832027+Axolem@users.noreply.github.com>
2022-09-23 02:26:32 +0200

    Fix

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import {Text, TextInput} from "react-native";
 Text.defaultProps = Text.defaultProps || {};
 Text.defaultProps.maxFontSizeMultiplier = 1; // the maximum amount the font size will scale.
 TextInput.defaultProps = Text.defaultProps || {};
-TextInput.defaultProps.maxFontSizeMultiplier = 1; /
+TextInput.defaultProps.maxFontSizeMultiplier = 1; 
 
 const App = () => {
   return (
