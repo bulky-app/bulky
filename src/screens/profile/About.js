c6cd70c45a62d97696cfd079f46bb237691cd19b
Axole Maranjana <axolemaranjana4@gmail.com>
2022-09-25 04:52:45 +0200

    Partially done

--- src/screens/profile/About.js
+++ src/screens/profile/About.js
@@ -1,11 +1,6 @@
 import styles from "../../globalStyles";
-import { useState, useEffect } from "react";
-import { SSButton } from "../../components/SButton";
-import { Text, View, StyleSheet } from "react-native";
-import HistoryItem from "../../components/HistoryItem";
-import { styless } from "../../components/ProfileCard";
+import { Text, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ScrollView } from "react-native-gesture-handler";
 
 const About = () => {
   return (
