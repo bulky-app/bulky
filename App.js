ac22d0d86d20d58c986b0a34fa7f2f1f8e8ec40a
Axole Maranjana <axolemaranjana4@gmail.com>
2022-09-25 02:40:05 +0200

    Clean up

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
-import "react-native-gesture-handler";
 import store from "./src/redux/store";
+import "react-native-gesture-handler";
 import { Provider } from "react-redux";
+import { Text, TextInput } from "react-native";
 import RootNavigator from "./src/navigation/RootNavigator";
-import Wallet from "./src/screens/profile/Wallet";
-import {Text, TextInput} from "react-native";
 
 Text.defaultProps = Text.defaultProps || {};
 Text.defaultProps.maxFontSizeMultiplier = 1; // the maximum amount the font size will scale.
 TextInput.defaultProps = Text.defaultProps || {};
-TextInput.defaultProps.maxFontSizeMultiplier = 1; 
+TextInput.defaultProps.maxFontSizeMultiplier = 1;
 
 const App = () => {
   return (
     <Provider store={store}>
-      {/*  <Wallet/>*/}
       <RootNavigator />
     </Provider>
   );
