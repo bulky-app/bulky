c4d30f7feacab310aeb7e516855e70036eae54db
junior-03 <juniormaruping6@gmail.com>
2022-10-09 13:38:31 +0200

    searchscreen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -11,23 +11,53 @@ import {
   Keyboard,
   TouchableOpacity,
   Platform,
+  Pressable,
+ FlatList
 } from "react-native";
 import { SearchBar } from "@rneui/base";
 
 const DATA = [
   {
-    title: "Search by Category :",
-    data: ["Canned food", "Meat", "Dairy", "Starch food"],
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "Canned food",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Meat",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Dairy",
+  },
+   {
+    id: "58694a0f-3da1-471f-bd96-145571e29d78",
+    title: "Starch food",
   },
 ];
-const Item = ({ title }) => (
-  <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
-  </View>
+const Item = ({ item, onPress, backgroundColor, textColor }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+    <Text style={[styles.title, textColor]}>{item.title}</Text>
+  </TouchableOpacity>
 );
 export default function SearchScreen() {
   const [searchBar, setSearchBar] = useState("");
-  const onChangeSearch = (query) => setSearchBar(query);
+  //const onChangeSearch = (query) => setSearchBar(query);
+    const [selectedId, setSelectedId] = useState(null);
+
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? "#A020F0" : "#dcdcdc";
+    const color = item.id === selectedId ? 'white' : 'black';
+
+
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+        backgroundColor={{ backgroundColor }}
+        textColor={{ color }}
+      />
+    );
+  };
   return (
     <SafeAreaView>
       <KeyboardAvoidingView
@@ -40,21 +70,18 @@ export default function SearchScreen() {
               placeholder="Search"
               lightTheme
               round
-              onChangeText={onChangeSearch}
               value={searchBar}
+              onChangeText={setSearchBar}
+              
             />
-            <View></View>
+            <View><Text style={styles.output}>{searchBar}</Text></View>
             <View>
-              <TouchableOpacity>
-                <SectionList
-                  sections={DATA}
-                  keyExtractor={(item, index) => item + index}
-                  renderItem={({ item }) => <Item title={item} />}
-                  renderSectionHeader={({ section: { title } }) => (
-                    <Text style={styles.header}>{title}</Text>
-                  )}
-                />
-              </TouchableOpacity>
+               <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        extraData={selectedId}
+      />
             </View>
           </View>
         </TouchableWithoutFeedback>
@@ -71,6 +98,12 @@ const styles = StyleSheet.create({
   },
 
   title: {
-    fontSize: 15,
+    fontSize: 10,
   },
+  output:{
+    padding :5,
+    backgroundColor: " #C5C6D0",
+    marginTop:4,
+  }
 });
+
