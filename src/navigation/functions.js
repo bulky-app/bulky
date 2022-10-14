196d54b04a7f53a40418b7301fc559734eeb84b6
Axole Maranjana <axolemaranjana4@gmail.com>
2022-10-14 03:36:02 +0200

    Added mailling funct

--- src/navigation/functions.js
+++ src/navigation/functions.js
@@ -1,5 +1,7 @@
-import { Alert } from "react-native";
+import emailjs from "@emailjs/browser";
 import Parse from "../../backend/server";
+import { Alert, ToastAndroid } from "react-native";
+import { EMAIL_JS_KEY } from "../../backend/env.vars";
 
 const validateEmail = (email) => {
   const validRegex =
@@ -22,4 +24,35 @@ const doUserPasswordReset = async function (email) {
       return false;
     });
 };
-export { validateEmail, doUserPasswordReset };
+
+const sendEmail = async (email, name, massage, alert) => {
+  await emailjs
+    .send(
+      "service_ilxto2k",
+      "template_96m2ygb",
+      {
+        to_name: name,
+        message: massage,
+        to_email: email,
+      },
+      EMAIL_JS_KEY
+    )
+    .then((response) => {
+      console.log(response);
+      if (alert === true) {
+        return Alert.alert(
+          "Success!",
+          `Please check your eail ${email} for further infomation.`
+        );
+      }
+      ToastAndroid.showWithGravityAndOffset(
+        "An email has been sent to you.",
+        ToastAndroid.LONG,
+        ToastAndroid.TOP,
+        25,
+        50
+      );
+    })
+    .catch((e) => console.log(e));
+};
+export { validateEmail, doUserPasswordReset, sendEmail };
