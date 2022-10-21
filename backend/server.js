e61391c8cc909db75ebdfb77cfab9bb01dcf9360
Axole Maranjana <axolemaranjana4@gmail.com>
2022-10-21 23:27:01 +0200

    kjy

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 // In a React Native application
 import Parse from "parse/react-native.js";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { APP_ID, JAVASCRIPT_KEY} from "./env.vars";
+import { APP_ID, JAVASCRIPT_KEY, SECRET_KEY ,publishable_Key} from "./env.vars";
 
 Parse.setAsyncStorage(AsyncStorage);
-Parse.initialize(APP_ID, JAVASCRIPT_KEY);
+Parse.initialize(APP_ID, JAVASCRIPT_KEY, SECRET_KEY, publishable_Key);
 Parse.serverURL = 'https://parseapi.back4app.com/';
 
 export default Parse;
