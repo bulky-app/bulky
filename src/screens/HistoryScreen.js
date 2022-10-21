7bd29652aacd991fa741cd724fcb7ab4e02cf207
Axole Maranjana <axolemaranjana4@gmail.com>
2022-10-22 01:02:31 +0200

    Update history everytime visited

--- src/screens/HistoryScreen.js
+++ src/screens/HistoryScreen.js
@@ -1,48 +1,51 @@
 import styles from "../globalStyles";
+import React, { useState } from "react";
 import Parse from "../../backend/server";
 import { StatusBar } from "expo-status-bar";
-import React, { useEffect, useState } from "react";
 import { EmptyHistory } from "../components/EmptyCart";
-import { useNavigation } from "@react-navigation/native";
 import { ScrollView } from "react-native-gesture-handler";
 import OrderHistoryItem from "../components/OrderHistoryItem";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 
 function HistoryScreen() {
   const navigation = useNavigation();
-  const [history, setHistory] = useState([])
+  const [history, setHistory] = useState([]);
+  const [active, setActive] = useState(false);
 
-  useEffect(() => {
-    const userHistoryGet = async () => {
-      try {
-        await Parse.User.currentAsync(); // Do not remove it Solves a certain error LOL.
-        const user = Parse.User.current();
-
-        const query = new Parse.Query("orders");
-        query.contains("userId", user.id); //filter for current user only
-        query.addAscending('createdAt'); //sort ascending
-        const queryResult = await query.find();
-        return setHistory(queryResult);
-      } catch (error) {
-        return error;
-      }
-    };
-    userHistoryGet();
-  }, []);
-
+  useFocusEffect(
+    React.useCallback(() => {
+      const userHistoryGet = async () => {
+        try {
+          await Parse.User.currentAsync(); // Do not remove it Solves a certain error LOL.
+          const user = Parse.User.current();
+          const query = new Parse.Query("orders");
+          query.contains("userID", user.id); //filter for current user only
+          const queryResult = await query.find();
+          return setHistory(queryResult);
+        } catch (error) {
+          return error;
+        }
+      };
+      return () => userHistoryGet();
+    }, [])
+  );
 
   return (
     <ScrollView nestedScrollEnabled={true} style={[styles.safeContainer]}>
       <StatusBar style="light" />
-      {history.length > 0 ? history.map((item) =>
-        <OrderHistoryItem
-          key={item.id}
-          amount={item.get("totalPrice")}
-          date={item.get("createdAt")}
-          status={item.get("orderStatus")}
-          id={item.id} />)
-        :
+      {history.length > 0 ? (
+        history.map((item) => (
+          <OrderHistoryItem
+            key={item.id}
+            amount={item.get("totalPrice")}
+            date={item.get("createdAt")}
+            status={item.get("orderStatus")}
+            id={item.id}
+          />
+        ))
+      ) : (
         <EmptyHistory onClick={() => navigation.navigate("Home")} />
-      }
+      )}
     </ScrollView>
   );
 }
