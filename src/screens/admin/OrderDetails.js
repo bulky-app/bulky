ee6e3c03616491ba6080e7a7ca22f080fdb3ed93
Axole Maranjana <axolemaranjana4@gmail.com>
2022-10-31 00:54:54 +0200

    complete

--- src/screens/admin/OrderDetails.js
+++ src/screens/admin/OrderDetails.js
@@ -5,33 +5,47 @@ import { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import SInput from "../../components/SInput";
 import { Feather } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useIsFocused, useNavigation } from "@react-navigation/native";
 import SelectDropdown from 'react-native-select-dropdown';
 import getDirections from "react-native-google-maps-directions";
 import LoadingButton, { CartButton } from "../../components/SButton";
 import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 import { StyleSheet, Text, View, Linking, KeyboardAvoidingView, Platform, ToastAndroid } from "react-native";
+import { useCallback } from "react";
+import { ActivityIndicator } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
 
 navigator.geolocation = require("react-native-geolocation-service");
 
 const OrderDetails = ({ route }) => {
+    const nav = useNavigation();
+    const isFocused = useIsFocused();
+
     const data = route.params;
     const objectId = data.get("userID").id;
     const orderStatus = data.get("orderStatus");
 
-    const [user, setUser] = useState();
-    const [address, setAddress] = useState({});
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [address, setAddress] = useState({ notes: "", streetAddress: "", resName: "" });
     const [location, setLocation] = useState({});
 
     const [comment, setComment] = useState("");
     const [status, setStatus] = useState(orderStatus);
-
+    const [loading, setLoading] = useState(true)
+    const [count, setCount] = useState(0)
 
     useEffect(() => {
         const getlocation = async () => {
             let { status } = await Location.requestForegroundPermissionsAsync();
             if (status !== 'granted') {
-                setErrorMsg('Permission to access location was denied');
+                ToastAndroid.showWithGravity(
+                    'Permission to access location was denied',
+                    ToastAndroid.SHORT,
+                    ToastAndroid.CENTER
+                )
                 return;
             }
             const location = await Location.getCurrentPositionAsync({});
@@ -40,17 +54,15 @@ const OrderDetails = ({ route }) => {
 
         const getUserAddress = async () => {
             const query = new Parse.Query("userAddresses");
-            query.contains("userId", objectId);
+            query.contains("userId", data.get("userID").id);
             try {
                 const queryResult = await query.find()
-                const userData = queryResult[0].get("userId")
-                setUser(
-                    {
-                        name: `${userData.get("name")} ${!!userData.get("surname")}`,
-                        email: userData.get("email"),
-                        phone: userData.get("phone")
-                    }
-                )
+                const updatedUserDetails = await Parse.Cloud.run("getUserDetails", {
+                    objectId
+                });
+                setName(`${updatedUserDetails.get("name")} ${updatedUserDetails.get("surname")}`)
+                setEmail(updatedUserDetails.get("email"))
+                setPhone(updatedUserDetails.get("phone"))
                 setAddress({
                     latitude: queryResult[0].get("location"),
                     streetAddress: queryResult[0].get("streetAddresses") + queryResult[0].get("suburbName"),
@@ -65,8 +77,9 @@ const OrderDetails = ({ route }) => {
         return () => {
             getlocation()
             getUserAddress()
+            return setLoading(false)
         }
-    }, [objectId])
+    }, [count])
 
     const handleGetDirections = () => {
         getDirections({
@@ -89,126 +102,156 @@ const OrderDetails = ({ route }) => {
         });
     };
 
-    const handleGetUpdate = () => {
-        console.log(data.id)
-    }
-    return (
-        <KeyboardAvoidingView
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-            style={localStyles.container}
-            enabled={true}
-            >
-            <StatusBar style="light" />
-            <View style={localStyles.card}>
-                <View>
-                    <Text style={localStyles.card.head}>Student Details. </Text>
-                </View>
-                <View style={localStyles.card.container}>
-                    <View style={localStyles.card.cardRow}>
-                        <Text style={localStyles.card.text}>
-                            Name:
-                            <Text style={localStyles.card.text.bold}>  {user?.name}</Text>
-                        </Text>
-                    </View>
-                    <View>
-                        <Text style={localStyles.card.text}>
-                            Student ID:
-                            <Text style={localStyles.card.text.bold}> {objectId}</Text>
-                        </Text>
-                    </View>
-                    <View>
-                        <Text style={localStyles.card.text}>
-                            Email:
-                            <Text style={localStyles.card.text.bold}>  {user?.email}</Text>
-                        </Text>
-                    </View>
-                    <View style={localStyles.card.cardRow}>
-                        <Text style={localStyles.card.text}>
-                            Phone:
-                            <Text style={localStyles.card.text.bold}>  {user?.phone}</Text>
-                        </Text>
-                        <TouchableWithoutFeedback
-                            onPress={async () => await Linking.openURL(`tel:${user?.phone}`)}
-                        >
-                            <Feather name="phone-outgoing" size={16} color={styles.purpleText.color} />
-                        </TouchableWithoutFeedback>
-                    </View>
-                </View>
-            </View>
-            <View style={localStyles.card}>
-                <View>
-                    <Text style={localStyles.card.head}>Address Details. </Text>
-                </View>
-                <View style={localStyles.card.container}>
-                    <View style={localStyles.card.cardRow}>
-                        <Text style={localStyles.card.text}>
-                            Residence Name:
-                            <Text style={localStyles.card.text.bold}> {address?.resName}</Text>
-                        </Text>
-                    </View>
-                    <View>
-                        <Text style={localStyles.card.text}>
-                            Street Address:
-                            <Text style={localStyles.card.text.bold}> {address?.streetAddress}</Text>
-                        </Text>
-                    </View>
-                    <View>
-                        <Text style={localStyles.card.text}>
-                            Notes:
-                            <Text style={localStyles.card.text.bold}> {address?.notes}
-                            </Text>
-                        </Text>
-                    </View>
-                    <View style={localStyles.card.cardRow}>
-                        <CartButton text="Directions" onPress={() => handleGetDirections()} />
-                    </View>
-                </View>
-            </View>
-            <View style={localStyles.card}>
-                <View>
-                    <Text style={localStyles.card.head}>Update Order. </Text>
-                </View>
-                <View style={localStyles.card.container}>
-                    <SelectDropdown
-                        data={["Awaiting Delivery", "Paid", "Canceled"]}
-                        defaultValue={status}
-                        onSelect={(selectedItem, index) => {
-                            setStatus(selectedItem)
-                        }}
-                        buttonStyle={
-                            {
-                                borderRadius: 10,
-                                padding: 0,
-                                height: 40,
-                                width: 240,
-                                marginTop: 10
-                            }
-                        }
-                        buttonTextStyle={{
-                            fontSize: 14,
-                            textAlign: "left"
-                        }}
-                        buttonTextAfterSelection={(selectedItem, index) => {
-                            return selectedItem
-                        }}
-                        rowTextForSelection={(item, index) => {
-                            return item
-                        }}
-                    />
-                    <SInput
-                        isSercure={false}
-                        keyboard="default"
-                        placeholderTxt="Comment"
-                        value={comment}
-                        handleChange={e => setComment(e)}
-                    />
-                </View>
+    const handleGetUpdate = async () => {
+        const update = new Parse.Object("orders");
 
-            </View>
-            <View style={localStyles.container.btn}>
-                <LoadingButton text="Directions" onPress={() => handleGetUpdate()} />
-            </View>
-        </KeyboardAvoidingView>
+        update.set('objectId', data.id);
+        update.set("orderStatus", status);
+        update.set("comment", comment);
+        try {
+            await update.save();
+            ToastAndroid.showWithGravity(
+                "Updated successfuly.",
+                ToastAndroid.SHORT,
+                ToastAndroid.SHORT
+            );
+            return nav.navigate('Orders');
+        } catch (error) {
+            console.log(error)
+            return ToastAndroid.showWithGravity(
+                "Failed to update please try again.",
+                ToastAndroid.SHORT,
+                ToastAndroid.SHORT
+            )
+        }
+    }
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setStatus(status);
+            console.log("Ran time out"),
+                setCount(prev => prev + 1)
+        }, 1000);
+        return () => {
+            if (count < 3) clearTimeout(timer);
+            return
+        };
+
+    }, []);
+    return (
+        <SafeAreaView>
+            <StatusBar style="light" />
+            {<View>
+                <View style={localStyles.card}>
+                    <View>
+                        <Text style={localStyles.card.head}>Student Details.</Text>
+                    </View>
+                    <View style={localStyles.card.container}>
+                        <View style={localStyles.card.cardRow}>
+                            <Text style={localStyles.card.text}>
+                                Name:
+                                <Text style={localStyles.card.text.bold}> {name}</Text>
+                            </Text>
+                        </View>
+                        <View>
+                            <Text style={localStyles.card.text}>
+                                Student ID:
+                                <Text style={localStyles.card.text.bold}> {objectId}</Text>
+                            </Text>
+                        </View>
+                        <View>
+                            <Text style={localStyles.card.text}>
+                                Email:
+                                <Text style={localStyles.card.text.bold}>  {email}</Text>
+                            </Text>
+                        </View>
+                        <View style={localStyles.card.cardRow}>
+                            <Text style={localStyles.card.text}>
+                                Phone:
+                                <Text style={localStyles.card.text.bold}>  {phone}</Text>
+                            </Text>
+                            <TouchableWithoutFeedback
+                                onPress={async () => await Linking.openURL(`tel:${phone}`)}
+                            >
+                                <Feather name="phone-outgoing" size={16} color={styles.purpleText.color} />
+                            </TouchableWithoutFeedback>
+                        </View>
+                    </View>
+                </View>
+                <View style={localStyles.card}>
+                    <View>
+                        <Text style={localStyles.card.head}>Address Details. </Text>
+                    </View>
+                    <View style={localStyles.card.container}>
+                        <View style={localStyles.card.cardRow}>
+                            <Text style={localStyles.card.text}>
+                                Residence Name:
+                                <Text style={localStyles.card.text.bold}> {address.resName}</Text>
+                            </Text>
+                        </View>
+                        <View>
+                            <Text style={localStyles.card.text}>
+                                Street Address:
+                                <Text style={localStyles.card.text.bold}> {address.streetAddress}</Text>
+                            </Text>
+                        </View>
+                        <View>
+                            <Text style={localStyles.card.text}>
+                                Notes:
+                                <Text style={localStyles.card.text.bold}> {address.notes} </Text>
+
+                            </Text>
+                        </View>
+                        <View style={localStyles.card.cardRow}>
+                            <CartButton text="Directions" onPress={() => handleGetDirections()} />
+                        </View>
+                    </View>
+                </View>
+                <View style={localStyles.card}>
+                    <View>
+                        <Text style={localStyles.card.head}>Update Order.</Text>
+                    </View>
+                    <View style={localStyles.card.container}>
+                        <SelectDropdown
+                            data={["Delivered", "Awaiting Delivery", "Paid", "Canceled"]}
+                            defaultValue={status}
+                            onSelect={(selectedItem, index) => {
+                                setStatus(selectedItem)
+                            }}
+                            buttonStyle={
+                                {
+                                    borderRadius: 10,
+                                    padding: 0,
+                                    height: 40,
+                                    width: 240,
+                                    marginTop: 10
+                                }
+                            }
+                            buttonTextStyle={{
+                                fontSize: 14,
+                                textAlign: "left"
+                            }}
+                            buttonTextAfterSelection={(selectedItem, index) => {
+                                return selectedItem
+                            }}
+                            rowTextForSelection={(item, index) => {
+                                return item
+                            }}
+                        />
+                        <SInput
+                            isSercure={false}
+                            keyboard="default"
+                            placeholderTxt="Comment"
+                            value={comment}
+                            handleChange={e => setComment(e)}
+                        />
+                    </View>
+
+                </View>
+                <View style={localStyles.container.btn}>
+                    <LoadingButton text="Directions" onPress={() => handleGetUpdate()} />
+                </View>
+            </View>}
+        </SafeAreaView>
     );
 };
 export default OrderDetails;
@@ -216,7 +259,7 @@ export default OrderDetails;
 const localStyles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 10
+        paddingTop: 10,
     },
     card: {
         elevation: 3,
--- src/screens/admin/Orders.js
+++ src/screens/admin/Orders.js
@@ -3,35 +3,63 @@ import { StatusBar } from "expo-status-bar";
 import Parse from "../../../backend/server";
 import { useState, useCallback } from "react";
 import { ScrollView } from "react-native-gesture-handler";
-import { useFocusEffect } from "@react-navigation/native";
+import { useFocusEffect, useIsFocused } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { OrderHistoryItemAdmin } from "../../components/OrderHistoryItem";
+import { ToastAndroid } from "react-native";
+import { ActivityIndicator } from "react-native";
+import styles from "../../globalStyles";
+import { useEffect } from "react";
 
 const Orders = () => {
     const [data, setData] = useState([])
-    const [update, setupdate] = useState(false)
+    const [loading, setLoading] = useState(true)
+    const isFocused = useIsFocused();
+    const [count, setCount] = useState(0)
 
     useFocusEffect(
         useCallback(() => {
+            setLoading(true)
             const getOrders = async () => {
                 const orders = new Parse.Query("orders");
+                const orders2 = new Parse.Query("orders");
                 orders.equalTo("orderStatus", "Paid");
+                orders2.equalTo("orderStatus", "Awaiting Delivery");
+                const query = Parse.Query.or(orders, orders2);
                 try {
-                    const response = await orders.find()
+                    const response = await query.find()
                     setData(response)
+                    return setLoading(false)
                 } catch (error) {
-                    Alert.alert("Error", error.massage)
+                    ToastAndroid.showWithGravity(
+                        "Failed getting orders.",
+                        ToastAndroid.SHORT,
+                        ToastAndroid.CENTER
+                    )
                 }
             }
-            return () => getOrders();
-        }, [update])
+            return () => { getOrders(); setLoading(false) }
+        }, [isFocused])
     );
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(loading)
+            console.log("Ran time out"),
+                setCount(prev => prev + 1)
+        }, 3000);
+        return () => {
+            if (count < 3) clearTimeout(timer);
+            return
+        };
+
+    }, []);
 
     return (
         <SafeAreaView>
             <StatusBar style="dark" />
             <ScrollView>
-                {data.length > 0 ?
+                { data.length > 0 ?
                     data.map(item =>
                         <OrderHistoryItemAdmin
                             id={item.id}
